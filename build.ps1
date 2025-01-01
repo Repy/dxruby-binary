bbd86a58461162e1a2a6df66a64a27e16f64c7fc
Repy <gamecube02+git@gmail.com>
2025-01-01 16:29:47 +0900

    messege

--- build.ps1
+++ build.ps1
@@ -1,16 +1,21 @@
 $ErrorActionPreference = "Stop"
 
+Write-Host "Install DirectX 9"
 Invoke-WebRequest "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe" -OutFile ".\directx_Jun2010_redist.exe"
 Start-Process -Wait -FilePath ".\directx_Jun2010_redist.exe" -ArgumentList "/Q", "/T:C:\directx"
 Start-Process -Wait -FilePath "C:\directx\DXSETUP.exe" -ArgumentList "/silent"
 
+Write-Host "Clone DXRuby"
 Start-Process -NoNewWindow -Wait -FilePath "git.exe" -ArgumentList "clone", "https://github.com/mirichi/dxruby.git"
 
 function Build-DXRuby($url, $installpath, $ver) {
+    Write-Host "Install Ruby $ver with Devkit"
     Invoke-WebRequest "$url" -OutFile ".\rubyinstaller.exe"
     Start-Process -Wait -FilePath ".\rubyinstaller.exe" -ArgumentList "/silent", "/currentuser"
     Remove-Item ".\rubyinstaller.exe"
     . "$installpath\bin\ridk.ps1" "enable"
+    
+    Write-Host "Build DXRuby for Ruby $ver"
     Start-Process -NoNewWindow -Wait -WorkingDirectory ".\dxruby\ext\dxruby" -FilePath "ruby.exe" -ArgumentList "extconf.rb"
     Start-Process -NoNewWindow -Wait -WorkingDirectory ".\dxruby\ext\dxruby" -FilePath "make.exe" -ArgumentList "CCDLFLAGS="" -fpermissive """
     New-Item -ItemType Directory ".\dxruby\lib\$ver"
@@ -23,14 +28,14 @@ Build-DXRuby "https://github.com/oneclick/rubyinstaller2/releases/download/RubyI
 Build-DXRuby "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.3.6-2/rubyinstaller-devkit-3.3.6-2-x86.exe" "C:\Ruby33" "3.3" 
 Build-DXRuby "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.4.1-1/rubyinstaller-devkit-3.4.1-1-x86.exe" "C:\Ruby34" "3.4"
 
-git tag -a "${TAGNAME}" -m "${TAGNAME} ${HEADSHA}" "${HEADSHA}"
-
+Write-Host "Build DXRuby Gem"
 Start-Process -NoNewWindow -Wait -WorkingDirectory ".\dxruby" -FilePath "git.exe" -ArgumentList "config", "--global", "user.email", "you@example.com"
 Start-Process -NoNewWindow -Wait -WorkingDirectory ".\dxruby" -FilePath "git.exe" -ArgumentList "config", "--global", "user.name", "Your Name"
 Start-Process -NoNewWindow -Wait -WorkingDirectory ".\dxruby" -FilePath "git.exe" -ArgumentList "add", "lib\*\*.so"
 Start-Process -NoNewWindow -Wait -WorkingDirectory ".\dxruby" -FilePath "git.exe" -ArgumentList "commit", "-m", "tmp"  
 Start-Process -NoNewWindow -Wait -WorkingDirectory ".\dxruby" -FilePath "gem.cmd" -ArgumentList "build", ".\dxruby.gemspec"
 
+Write-Host "Upload GitHub Release"
 $TAGNAME = Get-Date -Format "yyyyMMddHHmm"
 Start-Process -NoNewWindow -Wait -FilePath "git.exe" -ArgumentList "tag", "-a", "${TAGNAME}", "-m", "${TAGNAME}"
 Start-Process -NoNewWindow -Wait -FilePath "git.exe" -ArgumentList "push", "origin", "${TAGNAME}"
